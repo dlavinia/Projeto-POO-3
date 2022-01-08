a3f58bcc320cfaacc1f5b41831b474e15d6bce30
Débora Melo <56905620+dlavinia@users.noreply.github.com>
2022-01-08 00:29:22 +0000

    Aula 06

--- main.cs
+++ main.cs
@@ -10,11 +10,30 @@ class Program {
     Console.WriteLine (s3);
 
     Tarefa t1 = new Tarefa (1, "Cuidar de Porcos", "Cuidar dos 150 porcos da fazenda", 5, s2);
-    Tarefa t2 = new Tarefa (2, "Banhar os cavalos", "Dar banhos no 50 cavalos da fazenda", -2);
+    Tarefa t2 = new Tarefa (2, "Banhar os cavalos", "Dar banhos no 50 cavalos da fazenda", -2, s1);
     Tarefa t3 = new Tarefa (3, "Pastar os bois", "colocar as 345 cabeças de boi para o chiqueiro", 2, s3);
     Console.WriteLine (t1);
     Console.WriteLine (t2);
     Console.WriteLine (t3);
+
+    s1.TarefaInserir(t2);
+    s2.TarefaInserir(t1);
+    s1.TarefaInserir(t3);
+    Console.WriteLine();
+
+    Tarefa[] v = s1.TarefaListar();
+    Console.Write("Tarefas no setor ");
+    Console.WriteLine(s1.GetDescricao()+":");
+    foreach (Tarefa t in v)
+      Console.WriteLine(t);
+      Console.WriteLine();
+
+    v = s2.TarefaListar();
+    Console.Write("Tarefas no setor ");
+    Console.WriteLine(s2.GetDescricao()+":");
+    foreach (Tarefa t in v)
+      Console.WriteLine(t);
+      Console.WriteLine();
   }
 }
 
--- setor.cs
+++ setor.cs
@@ -3,6 +3,9 @@ using System;
 class Setor{
 	private int id;
 	private string descricao;
+  private Tarefa[] tarefas = new Tarefa[10];
+  private int nt;
+
 	public Setor(int id, string descricao){
 		this.id = id;
 		this.descricao = descricao;
@@ -20,7 +23,20 @@ class Setor{
   public void SetDescricao (string descricao){
 	  this.descricao = descricao;
   }
+  public void TarefaInserir(Tarefa t){
+    if (nt == tarefas.Length){
+	    Array.Resize(ref tarefas, 2 * tarefas.Length);
+    }
+	  tarefas[nt] = t;
+	  nt++;
+  }
+  public Tarefa[] TarefaListar(){
+	  Tarefa[] c = new Tarefa[nt];
+    Array.Copy(tarefas, c, nt);
+    return c;
+
+  }
   public override string ToString(){
-	return id + "-" + descricao;
+	return id + " - " + descricao +" - Nº tarefas " +nt;
   }
 }
