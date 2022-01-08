d8ceec39b03754f9c946b1c567cdcfdc3a136723
Débora Melo <56905620+dlavinia@users.noreply.github.com>
2022-01-08 12:19:22 +0000

    Aula 07

--- main.cs
+++ main.cs
@@ -2,39 +2,48 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Setor s1 = new Setor(1, "Cavalos");
-    Setor s2 = new Setor(2, "Porcos");
-    Setor s3 = new Setor(3, "Bois");
-    Console.WriteLine (s1);
-    Console.WriteLine (s2);
-    Console.WriteLine (s3);
+  int opc = 0;
+  Console.WriteLine("----- ORGANIZADOR DE TAREFAS -----");
+  do {
+    try{
+       opc = Menu();
+    switch(opc){
+      case 1: SetorListar(); break;
+      case 2: SetorInserir(); break;
+      }
+    } catch (Exception erro){
+      Console.WriteLine(erro.Message);
+      opc = 100;
 
-    Tarefa t1 = new Tarefa (1, "Cuidar de Porcos", "Cuidar dos 150 porcos da fazenda", 5, s2);
-    Tarefa t2 = new Tarefa (2, "Banhar os cavalos", "Dar banhos no 50 cavalos da fazenda", -2, s1);
-    Tarefa t3 = new Tarefa (3, "Pastar os bois", "colocar as 345 cabeças de boi para o chiqueiro", 2, s3);
-    Console.WriteLine (t1);
-    Console.WriteLine (t2);
-    Console.WriteLine (t3);
+    }
+   
+  } while(opc != 0);
 
-    s1.TarefaInserir(t2);
-    s2.TarefaInserir(t1);
-    s1.TarefaInserir(t3);
-    Console.WriteLine();
-
-    Tarefa[] v = s1.TarefaListar();
-    Console.Write("Tarefas no setor ");
-    Console.WriteLine(s1.GetDescricao()+":");
-    foreach (Tarefa t in v)
-      Console.WriteLine(t);
-      Console.WriteLine();
-
-    v = s2.TarefaListar();
-    Console.Write("Tarefas no setor ");
-    Console.WriteLine(s2.GetDescricao()+":");
-    foreach (Tarefa t in v)
-      Console.WriteLine(t);
-      Console.WriteLine();
+  Console.WriteLine("Aplicação finalizada :)");
   }
+
+  public static int Menu(){
+	  Console.WriteLine();
+    Console.WriteLine("---------------------");
+    Console.WriteLine("1 - Listar Setores");
+    Console.WriteLine("2 - Inserir Setor");
+    Console.WriteLine("0 - Finalizar");
+    Console.WriteLine("Infrome uma opção:");
+    int opc = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    return opc;
+}
+
+  public static void SetorListar(){
+    Console.WriteLine("----- TODOS OS SETORES -----");
+}
+
+  public static void SetorInserir(){
+    Console.WriteLine("----- INSERIR SETOR -----");
+
+}
+
+
 }
 
 
